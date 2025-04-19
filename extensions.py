44f3c87495546a4a0a88c9d31540c4716a1520d8
dmuench <businessdanielthedeveloper@gmail.com>
2025-04-19 15:06:04 +0200

    fix: Correct placement of docstring in extensions.py

--- extensions.py
+++ extensions.py
@@ -1,6 +1,7 @@
+"""Cache for FastAPI routes."""
+
 from cachetools import TTLCache
 
-"""Cache for FastAPI routes."""
 # Create a cache with a maximum size and time-to-live (TTL)
 cache = TTLCache(maxsize=5, ttl=60)  # Cache size of 5 items, expires after 1 minute
 cacheLong = TTLCache(maxsize=1, ttl=300)  # Cache size of 1 item, expires after 5 minutes
